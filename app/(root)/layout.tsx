152d0947de0ee86453687721b207497c6cab229e
Jericho <jerichozara.tmlcr@gmail.com>
2026-06-06 14:12:07 +0800

    feat: create reusable Loading page and implement boolean loading state logic

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,7 @@
+'use client';
+
+import { useEffect, useState } from "react";
+import Loading from "@/components/Loading";
 import Header from "@/components/Header";
 import Navbar from "@/components/Navbar";
 
@@ -6,12 +10,28 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [ isLoading, setIsLoading ] = useState<boolean>(true)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (isLoading) {
+    return <Loading />
+  }
+
   return (
-    <>
-        <Header>
-            <Navbar />
-        </Header>
-        {children}
-    </>
-  );
+  <>
+    <Header>
+      <Navbar />
+    </Header>
+
+    {children}
+        
+  </>
+);
 }
